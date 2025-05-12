ce0a6f26dd5cce38d72c7f20fc4662514b5c03bf
Evan Sawyer @ IVC <evan@ivc.media>
2025-05-12 10:37:46 -0700

    first commit

---
+++ src/llama_pinecone_mcp_0/__init__.py
@@ -0,0 +1,4 @@
+from .llama_pinecone_mcp_0 import mcp
+
+def main() -> None:
+    mcp.run()
---
+++ src/llama_pinecone_mcp_0/llama_pinecone_mcp_0.py
@@ -0,0 +1,51 @@
+from fastmcp import FastMCP
+import logging
+import sys
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core import StorageContext
+from llama_parse import LlamaParse
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv["PINECONE_API_KEY"]
+OPENAI_API_KEY = os.getenv["OPENAI_API_KEY"]
+LLAMA_CLOUD_API_KEY = os.getenv["LLAMA_CLOUD_API_KEY"]
+
+mcp = FastMCP("Pinecone")
+pc = Pinecone(api_key=PINECONE_API_KEY)
+pinecone_index = pc.Index("quickstart")
+
+parser = LlamaParse(
+    api_key=os.environ.get("LLAMA_CLOUD_API_KEY"),  # get API key from environment variables
+    result_type="markdown",  # "markdown" and "text" are available
+    verbose=True,
+)
+
+vector_store = PineconeVectorStore(pinecone_index.Index("cloudinary"))
+index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+query_engine = index.as_query_engine()
+
+@mcp.tool()
+def parse_documents(urls: list[str]) -> str:
+    """Parse a list of URLs and return the parsed result."""
+    return parser.load_data(urls)
+
+@mcp.tool()
+def upsert_documents(documents: list) -> str:
+    """Upsert a list of documents into the Pinecone index."""
+    vector_store = PineconeVectorStore(pinecone_index=pinecone_index, namespace='cloudinary')
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    VectorStoreIndex.from_documents(
+        documents, storage_context=storage_context)
+    return "Documents upserted successfully"
+
+@mcp.tool()
+def query_pinecone(query: str) -> str:
+    """Query the Pinecone index and return the results."""
+    return query_engine.query(query)
+
+
